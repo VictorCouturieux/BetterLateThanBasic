d76341bcd5ad3c431242fde3abd99811790df3e6
Elootam <toma.lion@hotmail.fr>
2022-12-10 18:41:59 +0100

    that for sure a better carController !

--- Assets/ColdResources/Thomas's Scrap/Scripts/CarController.cs
+++ Assets/ColdResources/Thomas's Scrap/Scripts/CarController.cs
@@ -12,39 +12,49 @@ public class CarController : MonoBehaviour
     [Header("suspension setup")]
     [SerializeField] private float springStrength = 50;
     [SerializeField] private float springDamper = 5;
-    [SerializeField] private float springOffsef = 1;
+    [SerializeField] private float springOffsef = 1.5f;
 
     [Space] 
     [Header("steering setup")] 
-    [SerializeField] private float frontGripFactor = .30f;
-    [SerializeField] private float rearGripFactor = .5f;
-    [SerializeField] private float tireMass = 1;
+    [SerializeField] private float frontGripFactor = 40;
+    [SerializeField] private float rearGripFactor = 40;
+    [SerializeField] private float tireMass = .01f;
 
     [Space] [Header("acceleration setup")] 
     [SerializeField] private float carTopSpeed = 15;
-    [SerializeField] private float drag = 3;
     [SerializeField] private float speed = 15;
     [SerializeField] private AnimationCurve powerCurve;
+    [SerializeField] private float drag = 2;
 
     [Space] [Header("rotation angle setup")] 
     [SerializeField] private float maxWheelAngle = 30;
     
-    private Rigidbody rb;
+    [Space] [Header("others setup")] 
+    [SerializeField] private float carMass = 30;
+    
+    private Rigidbody carRigidbody;
     private float forwardInput;
     private float rightInput;
     private float tireGripFactor;
+    private float raycastDistance;
+    
+    //gravity settings
+    private const float GRAVITATIONAL_ACCELERATION = 9.81f;
+    
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        carRigidbody = GetComponent<Rigidbody>();
         if (tireTransforms.Count < 1) Debug.Log("tireTransforms is empty, please assign tires !");
+        raycastDistance = springOffsef + 1;
     }
 
     private void Update()
     {
+        int raycastFailedHit = 0;
         for (int i = 0; i < tireTransforms.Count; i++)
         {
             Vector3 downDir = -Vector3.up;
-            bool rayDidHit = Physics.Raycast(tireTransforms[i].position, downDir, out RaycastHit tireRay, 20);
+            bool rayDidHit = Physics.Raycast(tireTransforms[i].position, downDir, out RaycastHit tireRay, raycastDistance);
             if (i < 2)
             {
                 tireTransforms[i].localRotation = Quaternion.Euler(0,rightInput * maxWheelAngle,0);
@@ -61,7 +71,7 @@ public class CarController : MonoBehaviour
                 Vector3 springDir = tireTransforms[i].up;
                 
                 //world-space velocity of this tire
-                Vector3 tireWorldVel = rb.GetPointVelocity(tireTransforms[i].position);
+                Vector3 tireWorldVel = carRigidbody.GetPointVelocity(tireTransforms[i].position);
                 
                 //calculate offset from raycast
                 float offset = springOffsef - tireRay.distance;
@@ -75,7 +85,7 @@ public class CarController : MonoBehaviour
                 float force = (offset * springStrength) - (vel * springDamper);
                 
                 //apply the force at the location of this tire, in the direction of the suspension
-                rb.AddForceAtPosition(springDir * force,tireTransforms[i].position);
+                carRigidbody.AddForceAtPosition(springDir * force,tireTransforms[i].position);
                 
                 Debug.DrawRay(tireTransforms[i].position, springDir * vel, Color.green);
             }
@@ -87,7 +97,7 @@ public class CarController : MonoBehaviour
                 Vector3 steeringDir = tireTransforms[i].right;
                 
                 //world-space velocity of the suspension
-                Vector3 tireWorldVel = rb.GetPointVelocity(tireTransforms[i].position);
+                Vector3 tireWorldVel = carRigidbody.GetPointVelocity(tireTransforms[i].position);
 
                 //what is the tire's velocity in the steering direction?
                 //note that steeringDir is a unit vector, so this return the magnitude of of tireWorldVel
@@ -103,7 +113,7 @@ public class CarController : MonoBehaviour
                 float desiredAccel = desiredeVelChange / Time.fixedDeltaTime;
                 
                 //Force = Mass * Acceleration, so multiply by the mass of the tire and apply it as a force
-                rb.AddForceAtPosition(steeringDir * tireMass * desiredAccel, tireTransforms[i].position);
+                carRigidbody.AddForceAtPosition(steeringDir * tireMass * desiredAccel, tireTransforms[i].position);
 
                 Debug.DrawRay(tireTransforms[i].position, steeringDir * steeringVel, Color.red);
             }
@@ -113,28 +123,45 @@ public class CarController : MonoBehaviour
             {
                 //world-space direction of the acceleration/braking force
                 Vector3 accelDir = tireTransforms[i].forward;
-                
+
                 //acceleration torque
                 if (Mathf.Abs(forwardInput) > 0)
                 {
-                    rb.drag = .3f;
+                    carRigidbody.drag = .3f;
                     //forward speed of the car (in the direction of driving)
-                    float carSpeed = Vector3.Dot( transform.forward, rb.velocity);
-                    
+                    float carSpeed = Vector3.Dot(transform.forward, carRigidbody.velocity);
+
                     //normalized car speed
                     float normalizedSpeed = Mathf.Clamp01(Mathf.Abs(carSpeed) / carTopSpeed);
-                    
+
                     //aviable torque
                     float aviableTorque = powerCurve.Evaluate(normalizedSpeed) * forwardInput;
-                    
-                    rb.AddForceAtPosition(accelDir * aviableTorque * speed, tireTransforms[i].position);
-                    
+
+                    carRigidbody.AddForceAtPosition(accelDir * aviableTorque * speed, tireTransforms[i].position);
+
                     Debug.DrawRay(tireTransforms[i].position, accelDir * carSpeed, Color.blue);
                 }
                 else
                 {
-                    rb.drag = drag;
+                    carRigidbody.drag = drag;
                 }
+            }
+
+            //if raycast didn't hit the ground, apply gravity on car at each tire position that didn't hit the ground
+            if (!rayDidHit)
+            {
+                // Calculate the gravitational force acting on the object (in Newtons)
+                float gravitationalForce = GRAVITATIONAL_ACCELERATION * carMass;
+
+                // Update the acceleration of the object using the gravitational force
+                Vector3 acceleration = new Vector3(0, -gravitationalForce, 0);
+
+                // Update the velocity of the object using the current acceleration
+                Vector3 velocity = (carRigidbody.velocity + acceleration) * Time.deltaTime;
+                
+                carRigidbody.AddForceAtPosition(velocity, tireTransforms[i].position);
+
+                Debug.DrawRay(tireTransforms[i].position, velocity, Color.yellow);
             }
         }
     }
