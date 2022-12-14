a1f84d1502eefd38192ea7062ae08af7d95addfd
Elootam <toma.lion@hotmail.fr>
2022-12-14 10:40:11 +0100

    better cote + less savonette :v

--- Assets/ColdResources/Thomas's Scrap/Scripts/BodyCarPartsManager.cs
+++ Assets/ColdResources/Thomas's Scrap/Scripts/BodyCarPartsManager.cs
@@ -1,29 +1,59 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Unity.Mathematics;
 using UnityEngine;
-using UnityEngine.InputSystem;
 using Random = UnityEngine.Random;
 
 public class BodyCarPartsManager : MonoBehaviour
 {
-    [Header("place the parent object where to set body car parts")] [SerializeField]
-    private GameObject visual;
+    [Header("place the parent object where to set body car parts")] 
+    [SerializeField] private GameObject visual;
 
-    [Space] [Header("Settings")] [SerializeField]
-    private float carSensibilityToHit = 50f;
+    [Space] [Header("Settings")] 
+    [SerializeField] private float carSensibilityToHit = 50f;
 
     [HideInInspector] public int playerScore;
 
     private Rigidbody carRigidbody;
     private List<GameObject> addedBodyCarParts = new();
     private int[] nunberOfEachBodyCarPart = new int[5];
+    private GameObject[] tireArrray = new GameObject[4];
 
-    private void Start() => carRigidbody = transform.GetComponent<Rigidbody>();
+    private void Start()
+    {
+        carRigidbody = transform.GetComponent<Rigidbody>();
+        for (int i = 1; i < 5; i++) tireArrray[i - 1] = transform.GetChild(0).transform.GetChild(i).gameObject;
+    }
 
+    private void LoseARandomBodyCarPArt()
+    {
+        //vibrate a lot the gamepad when player hit a fence
+        //StartCoroutine(StartAndStopVibration(.5f, 1f));
 
+        if (addedBodyCarParts.Count > 0)
+        {
+            int rnd = Random.Range(0, addedBodyCarParts.Count);
+            addedBodyCarParts[rnd].transform.DetachChildren();
+            addedBodyCarParts[rnd].GetComponent<Rigidbody>().AddForce(Vector3.up * 50);
+            Destroy(addedBodyCarParts[rnd], 2.5f);
+
+            if (addedBodyCarParts[rnd].name.Contains("Wheel"))
+                for (int i = 0; i < tireArrray.Length; i++)
+                    tireArrray[i].SetActive(false);
+
+            for (int i = 0; i < addedBodyCarParts.Count; i++)
+            {
+                if (addedBodyCarParts[i] == addedBodyCarParts[rnd])
+                {
+                    playerScore -= 500 * nunberOfEachBodyCarPart[i];
+                    nunberOfEachBodyCarPart[i] = 0;
+                }
+            }
+
+            addedBodyCarParts.RemoveAt(rnd);
+        }
+    }
+    
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.transform.CompareTag("Player"))
@@ -36,70 +66,25 @@ public class BodyCarPartsManager : MonoBehaviour
             if (otherCarMagnitude > myMagnitude)
             {
                 if (otherCarMagnitude - myMagnitude >= carSensibilityToHit / 2)
-                {
-                    //vibrate a lot the gamepad when player hit another car
-                    //StartCoroutine(StartAndStopVibration(.5f, 1f));
-                    
-                    if (addedBodyCarParts.Count > 0)
-                    {
-                        int rnd = Random.Range(0, addedBodyCarParts.Count);
-                        addedBodyCarParts[rnd].transform.DetachChildren();
-                        addedBodyCarParts[rnd].GetComponent<Rigidbody>().AddForce(Vector3.up * 50);
-                        Destroy( addedBodyCarParts[rnd],2.5f);
-                        
-                        for (int i = 0; i < addedBodyCarParts.Count; i++)
-                        {
-                            if (addedBodyCarParts[i] == addedBodyCarParts[rnd])
-                            {
-                                playerScore -= 500 * nunberOfEachBodyCarPart[i];
-                                nunberOfEachBodyCarPart[i] = 0;
-                            }
-                        }
-
-                        addedBodyCarParts.RemoveAt(rnd);
-                    }
-                }
+                    LoseARandomBodyCarPArt();
             }
-            else if (collision.transform.CompareTag("Barrier") &&
-                     carRigidbody.velocity.magnitude >= carSensibilityToHit)
-            {
-                //vibrate a lot the gamepad when player hit a fence
-                //StartCoroutine(StartAndStopVibration(.5f, 1f));
-                
-                if (addedBodyCarParts.Count > 0)
-                {
-                    int rnd = Random.Range(0, addedBodyCarParts.Count);
-                    addedBodyCarParts[rnd].transform.DetachChildren();
-                    addedBodyCarParts[rnd].GetComponent<Rigidbody>().AddForce(Vector3.up * 50);
-                    Destroy( addedBodyCarParts[rnd],2.5f);
-
-                    for (int i = 0; i < addedBodyCarParts.Count; i++)
-                    {
-                        if (addedBodyCarParts[i] == addedBodyCarParts[rnd])
-                        {
-                            playerScore -= 500 * nunberOfEachBodyCarPart[i];
-                            nunberOfEachBodyCarPart[i] = 0;
-                        }
-                    }
-
-                    addedBodyCarParts.RemoveAt(rnd);
-                }
-            }
+            else if (collision.transform.CompareTag("Barrier") && carRigidbody.velocity.magnitude >= carSensibilityToHit) 
+                LoseARandomBodyCarPArt();
         }
     }
-
+    
     private void OnTriggerEnter(Collider other)
     {
-        if (other.transform.CompareTag("BodyCarPart"))
+        if (other.transform.CompareTag("BodyCarPart")) //trigger a loot box
         {
             //vibrate a little the gamepad when player drive on a collectible
             //StartCoroutine(StartAndStopVibration(1f, .25f));
-            
-            //trigger a loot box
-            other.GetComponent<BodyCarCollectibleSpawner>().OnCollisionWithCar();
+
             var currentCollectibleSpawner = other.GetComponent<BodyCarCollectibleSpawner>();
             bool isAlreadyOnCar = false;
 
+            currentCollectibleSpawner.OnCollisionWithCar();
+            
             foreach (GameObject element in addedBodyCarParts.Where(element =>
                          element.name == currentCollectibleSpawner
                              .bodyCarParts[currentCollectibleSpawner.choosenLoot]
@@ -108,13 +93,15 @@ public class BodyCarPartsManager : MonoBehaviour
 
             if (!isAlreadyOnCar)
             {
-                GameObject go = Instantiate(
-                    currentCollectibleSpawner.bodyCarParts[currentCollectibleSpawner.choosenLoot],
-                    transform.position,
-                    quaternion.identity, visual.transform);
+                GameObject go = Instantiate(currentCollectibleSpawner.bodyCarParts[currentCollectibleSpawner.choosenLoot],
+                    transform.position, quaternion.identity, visual.transform);
+                
                 go.transform.localRotation = Quaternion.Euler(0, 0, 0);
                 go.name = currentCollectibleSpawner.bodyCarParts[currentCollectibleSpawner.choosenLoot].name;
                 addedBodyCarParts.Add(go);
+
+                if (go.name.Contains("Wheel"))
+                    for (int i = 0; i < tireArrray.Length; i++) tireArrray[i].SetActive(false);
             }
 
             playerScore += 500;
